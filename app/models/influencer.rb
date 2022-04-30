a910c2fc5eaf1d2e9486a33ba66053c95044c715
carlos diaz <diazmelian.carlos@gmail.com>
2022-04-30 14:01:02 +0100

    created validations

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,3 +1,8 @@
 class Influencer < ApplicationRecord
   belongs_to :platform
+
+  validates :handle, presence: true
+  validates :followers, presence: true
+  validates :profile_pic_url, presence: true
+  validates :primary_tag_id, presence: true
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,3 +1,7 @@
 class Platform < ApplicationRecord
   has_many :influencers
+
+  validates :name, presence: true
+  validates :base_url, presence: true
+
 end
